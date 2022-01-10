bda46da0663cf7b30f6d86236aa8170cf5b4d358
Bart Bucknill <bartbucknill@hotmail.com>
2022-01-10 16:34:16 +0000

    refactor UpdateQuality func

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -5,52 +5,50 @@ type Item struct {
 	sellIn, quality int
 }
 
+const MAX_QUALITY = 50
+
 func UpdateQuality(items []*Item) {
-	for i := 0; i < len(items); i++ {
-
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
-			}
+	for _, item := range items {
+		//UPDATE SELLIN
+		if item.name != "Sulfuras, Hand of Ragnaros" {
+			item.sellIn--
 		}
-
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
-				}
+		// UPDATE QUALITY
+		switch item.name {
+		case "Aged Brie":
+			if item.quality >= MAX_QUALITY {
+				continue
+			}
+			if item.sellIn < 0 {
+				item.quality += 2
 			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
+				item.quality++
+			}
+		case "Backstage passes to a TAFKAL80ETC concert":
+			if item.quality >= MAX_QUALITY {
+				continue
+			}
+			if item.sellIn > 0 {
+				if item.sellIn < 5 {
+					item.quality += 3
+				} else if item.sellIn < 10 {
+					item.quality += 2
+				} else {
+					item.quality++
 				}
+			} else if item.sellIn < 0 {
+				item.quality = 0
+			}
+		case "Sulfuras, Hand of Ragnaros":
+		default:
+			if item.quality <= 0 {
+				continue
+			}
+			if item.quality > 0 {
+				item.quality--
+			}
+			if item.sellIn < 0 && item.quality > 0 {
+				item.quality = item.quality - 1
 			}
 		}
 	}
