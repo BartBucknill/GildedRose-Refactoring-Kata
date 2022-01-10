312e69eb351f4046aa18766bb1a908d3056e39fc
Bart Bucknill <bartbucknill@hotmail.com>
2022-01-10 16:59:14 +0000

    add Conjured feature

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -9,12 +9,18 @@ const MAX_QUALITY = 50
 
 func UpdateQuality(items []*Item) {
 	for _, item := range items {
-		//UPDATE SELLIN
+
+		//UPDATE SELLIN: same for all items except Sulfuras
 		if item.name != "Sulfuras, Hand of Ragnaros" {
 			item.sellIn--
 		}
-		// UPDATE QUALITY
+
+		// UPDATE QUALITY: differs significantly, this switch statement treats each item type
+		// separately so allowing a single, independent policy with simple logic for each
+		// at the cost of some minor duplication
 		switch item.name {
+		case "Sulfuras, Hand of Ragnaros":
+			//make no changes for this case
 		case "Aged Brie":
 			if item.quality >= MAX_QUALITY {
 				continue
@@ -39,7 +45,26 @@ func UpdateQuality(items []*Item) {
 			} else if item.sellIn < 0 {
 				item.quality = 0
 			}
-		case "Sulfuras, Hand of Ragnaros":
+		case "Conjured":
+			// Conjured items decrease in quality at twice the speed of default items
+			// meaning a loss of 2 quality when sellIn > 0, and of 4 when sellIn > 0
+			// but as with default, quality should never be negative
+			if item.quality <= 0 {
+				continue
+			}
+			if item.sellIn < 0 {
+				if item.quality >= 4 {
+					item.quality -= 4
+				} else {
+					item.quality = 0
+				}
+			} else {
+				if item.quality >= 2 {
+					item.quality -= 2
+				} else {
+					item.quality = 0
+				}
+			}
 		default:
 			if item.quality <= 0 {
 				continue
@@ -48,7 +73,7 @@ func UpdateQuality(items []*Item) {
 				item.quality--
 			}
 			if item.sellIn < 0 && item.quality > 0 {
-				item.quality = item.quality - 1
+				item.quality--
 			}
 		}
 	}
